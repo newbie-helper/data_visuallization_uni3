0ea3c72f4b09db36f1e653e0173edd1de3719c51
ms1ms2ms <asd8335@naver.com>
2024-10-15 14:12:49 +0900

    Update new.py

--- new.py
+++ new.py
@@ -30,3 +30,21 @@ st.sidebar.checkbox('체크박스2')
 st.sidebar.radio('라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])
 st.sidebar.selectbox('셀렉트 박스', ['select 1', 'select 2', 'select 3'])
 
+#Layout --------------------------------
+# 레이아웃: 컬럼
+st.header('컬럼 레이아웃')
+col_1,col_2,col_3=st.columns([1,2,1]) # 컬럼 인스턴스 생성. 1:2:1 비율로 컬럼을 나눔
+
+with col_1:
+    st.write('## 1번 컬럼')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 1')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 2')
+
+with col_2:
+    st.write('## 2번 칼럼')
+    st.radio('2번 칼럼의 라디오 버튼',['radio 1','radio 2', 'radio 3']) #동일한 라디오 버튼을 생성할 수 없음
+    # 사이드바에  이미 라디오 버튼이 생성되어 있기 때문에, 여기서는 라디오 버튼의 내용을 변경해야 오류가 발생하지 않음
+
+col_3.wirte('## 3번 컬럼')
+col_3.selectbox('3번 컬럼의 셀렉트박스', ['select 1', 'select 2','select 3'])
+# 사이드바에 이미 셀렉트박스가 생성되어 있기 때문에, 여기서는 셀렉트박스의 내용을 변경해야 오류가 발생하지 않음
