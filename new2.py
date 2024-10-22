d68a2d65f14094377b3156c6af3711cdddaa40ce
ms1ms2ms <asd8335@naver.com>
2024-10-22 09:39:45 +0900

    Update new2.py

--- new2.py
+++ new2.py
@@ -13,12 +13,12 @@ st.write(f'입력된 텍스트:{text}')
 
 #숫자 입력은 입력된 값을 변환
 
-st.write('#### : orange[숫자 입력]')
+st.write('#### :orange[숫자 입력]')
 number = st.number_input('여기에 숫자를 입력하세요')
 st.write(f'입력된 숫자:{number}')
 
 #날짜 입력은 입력된 값을 반환
-st.write('####:orange[날짜 입력]')
+st.write('#### :orange[날짜 입력]')
 date = st.date_input('날짜를 선택하세요')
 st.write(f'선택된 날짜: {date}')
 
