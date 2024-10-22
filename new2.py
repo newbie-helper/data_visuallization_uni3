a89e4ace50c41bd95c06a1dfc0dae4325c3e09d4
ms1ms2ms <asd8335@naver.com>
2024-10-22 09:38:26 +0900

    Update new2.py

--- new2.py
+++ new2.py
@@ -13,7 +13,7 @@ st.write(f'입력된 텍스트:{text}')
 
 #숫자 입력은 입력된 값을 변환
 
-st.write('#### : orange[날짜 입력]')
+st.write('#### : orange[숫자 입력]')
 number = st.number_input('여기에 숫자를 입력하세요')
 st.write(f'입력된 숫자:{number}')
 
